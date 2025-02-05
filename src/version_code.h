be265ab3a2b6271ce9a328e1ffb4a14ca956b11c
insoft <developer@insoft.uk>
2025-02-05 20:41:49 +0000

    Update

--- src/version_code.h
+++ src/version_code.h
@@ -21,8 +21,8 @@
 // SOFTWARE.
 
 #define VERSION_NUMBER        "1.0.8"
-#define VERSION_CODE          "A0I74-25B3"
-#define NUMERIC_BUILD          10874
-#define INTERNAL_BUILD_CODE   "A0I74"
-#define DATE                  "2025 February 04"
+#define VERSION_CODE          "A0I80-25B4"
+#define NUMERIC_BUILD          10880
+#define INTERNAL_BUILD_CODE   "A0I80"
+#define DATE                  "2025 February 05"
 #define YEAR                  "2025"
